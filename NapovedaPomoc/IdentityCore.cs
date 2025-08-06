4393259f6d83181daf6b627cd4a05949554fb106
Martin <56979879+Martin74Cs@users.noreply.github.com>
2025-08-06 14:29:21 +0200

    Create IdentityCore.cs
    Jedna

---
+++ NapovedaPomoc/IdentityCore.cs
@@ -0,0 +1,14 @@
+builder.Services.AddIdentityCore<ApplicationUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+        options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 3;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
